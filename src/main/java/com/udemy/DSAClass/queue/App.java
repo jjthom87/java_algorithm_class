cddcd25f23b6d1214df82860a861b0e889bbcc54
Jared Thomas <jjthom87@yahoo.com>
2018-03-26 06:56:27 -0400

    adding queue

--- src/main/java/com/udemy/DSAClass/queue/App.java
+++ src/main/java/com/udemy/DSAClass/queue/App.java
@@ -9,6 +9,7 @@ public class App {
 		myQueue.insert(100);
 		myQueue.insert(100);
 		myQueue.insert(100);
+		myQueue.insert(100);
 		myQueue.view();
 	}
 
--- src/main/java/com/udemy/DSAClass/queue/Queue.java
+++ src/main/java/com/udemy/DSAClass/queue/Queue.java
@@ -17,13 +17,34 @@ public class Queue {
 	}
 	
 	public void insert(long j) {
+		if(rear == maxSize -1) {
+			rear = -1;
+		}
 		rear++;
 		queueArray[rear] = j;
 		nItems++;
 	}
 	
-	public void remove() {
-		
+	public long remove() { //remove item from the front of queue
+		long temp = queueArray[front];
+		front++;
+		if(front == maxSize) {
+			front = 0; // we can set front back to the 0th index so that we can utilize the entire array again
+		}
+		nItems--;
+		return temp;
+	}
+	
+	public long peekFront() {
+		return queueArray[front];
+	}
+	
+	public boolean isEmpty() {
+		return nItems == 0;
+	}
+	
+	public boolean isFull() {
+		return nItems == maxSize;
 	}
 	
 	public void view() {
