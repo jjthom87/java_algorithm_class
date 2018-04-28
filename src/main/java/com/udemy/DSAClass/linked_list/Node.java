23c43b0845956efdec8da61adf2608d374f39551
Jared Thomas <jjthom87@yahoo.com>
2018-04-28 02:07:51 -0400

    linked list intro

---
+++ src/main/java/com/udemy/DSAClass/linked_list/Node.java
@@ -0,0 +1,6 @@
+package com.udemy.DSAClass.linked_list;
+
+public class Node {
+	int data;
+	Node next = null;
+}
--- src/main/java/com/udemy/DSAClass/queue/App.java
+++ src/main/java/com/udemy/DSAClass/queue/App.java
@@ -5,11 +5,12 @@ public class App {
 	public static void main(String[] args) {
 		Queue myQueue = new Queue(5);
 		myQueue.insert(100);
-		myQueue.insert(100);
-		myQueue.insert(100);
-		myQueue.insert(100);
-		myQueue.insert(100);
-		myQueue.insert(100);
+		myQueue.insert(90);
+		myQueue.insert(80);
+		myQueue.insert(70);
+		myQueue.insert(60);
+		myQueue.view();
+		myQueue.remove();
 		myQueue.view();
 	}
 
